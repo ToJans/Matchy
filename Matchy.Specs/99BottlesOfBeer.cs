e3ac9f126bbd06cee9dfbe52e5dd311ec4e592a2
Tom Janssens <Tom@corebvba.be>
2013-09-07 07:21:21 +0200

    One bottle of beer on the wall

--- Matchy.Specs/99BottlesOfBeer.cs
+++ Matchy.Specs/99BottlesOfBeer.cs
@@ -9,14 +9,25 @@ namespace Matchy.Specs
     [TestClass]
     public class _99BottlesOfBeer
     {
-        Def verse;
+        Def verse,bottles,lastOne;
 
         [TestInitialize]
         public void Setup()
         {
             verse = new Def()
-                .Match(8).With( "8 bottles of beer on the wall, 8 bottles of beer.\n" +
-                                "Take one down and pass it around, 7 bottles of beer on the wall.\n");
+                .Match<int>().With( n => string.Format(
+                    "{0} of beer on the wall, {0} of beer.\n" +
+                    "Take {1} down and pass it around, {2} of beer on the wall.\n"
+                    ,bottles[n],lastOne[n],bottles[n-1]));
+
+            lastOne = new Def()
+                .Match(1).With("it")
+                .Match<int>().With("one");
+
+            bottles = new Def()
+                .Match(0).With("no more bottles")
+                .Match(1).With("1 bottle")
+                .Match<int>().With(n=>string.Format("{0} bottles",n));
         }
 
         [TestMethod]
@@ -26,6 +37,15 @@ namespace Matchy.Specs
                 "8 bottles of beer on the wall, 8 bottles of beer.\n" + 
                 "Take one down and pass it around, 7 bottles of beer on the wall.\n";
             Assert.AreEqual(Expected,verse[8]);
+        }
+
+        [TestMethod]
+        public void Verse_1()
+        {
+            var Expected =
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "Take it down and pass it around, no more bottles of beer on the wall.\n";
+            Assert.AreEqual(Expected, verse[1]);
         }
     }
 }
