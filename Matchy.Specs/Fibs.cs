0ee206ccc02baba779723586be3bf6ab73e80ad2
Tom Janssens <Tom@corebvba.be>
2013-09-07 05:41:23 +0200

    A working test! Woohoo
    Removed shouldly, as this does not work well combined with dynamic

--- Matchy.Specs/Fibs.cs
+++ Matchy.Specs/Fibs.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Matchy.Specs
 {
@@ -11,7 +10,8 @@ namespace Matchy.Specs
         [TestInitialize]
         public void SetupFibs()
         {
-            fib.match(0).with(0)
+            fib = new Def()
+               .match(0).with(0)
                .match(1).with(1)
                .match<int>().with(n => fib[n-1]*fib[n-2]);
         }
@@ -19,7 +19,7 @@ namespace Matchy.Specs
         [TestMethod]
         public void Fib_0()
         {
-            fib[0].ShouldBe(0);
+            Assert.AreEqual(0,fib[0]);
         }
     }
 }
---
+++ Matchy/Def.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Matchy
+{
+    public class Def:IMatch
+    {
+
+        public IMatched<T> match<T>()
+        {
+            return new Matched<T>(this);
+        }
+
+        public IMatched<T> match<T>(T what)
+        {
+            return new Matched<T>(this);
+        }
+
+        public IMatched<T> match<T>(Predicate<T> what)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic this[dynamic value]
+        {
+            get { return 0; }
+        }
+
+        class Matched<T>:IMatched<T>
+        {
+            public IMatch parent;
+
+            public Matched(IMatch parent)
+            {
+                this.parent = parent;
+            }
+
+            public IMatch with(T what)
+            {
+                return parent;
+            }
+
+            public IMatch with(Func<T, dynamic> what)
+            {
+                return parent;
+            }
+        }
+
+
+    }
+}
