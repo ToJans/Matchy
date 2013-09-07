2f2d9f4bec340cb9db1d5ca37d352b2f36fd790e
Tom Janssens <Tom@corebvba.be>
2013-09-07 07:29:58 +0200

    no more bottles of beer

--- Matchy.Specs/99BottlesOfBeer.cs
+++ Matchy.Specs/99BottlesOfBeer.cs
@@ -9,16 +9,19 @@ namespace Matchy.Specs
     [TestClass]
     public class _99BottlesOfBeer
     {
-        Def verse,bottles,lastOne;
+        Def verse, bottles, lastOne;
 
         [TestInitialize]
         public void Setup()
         {
             verse = new Def()
-                .Match<int>().With( n => string.Format(
+                .Match(0).With(
+                    "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                    "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+                .Match<int>().With(n => string.Format(
                     "{0} of beer on the wall, {0} of beer.\n" +
                     "Take {1} down and pass it around, {2} of beer on the wall.\n"
-                    ,bottles[n],lastOne[n],bottles[n-1]));
+                    , bottles[n], lastOne[n], bottles[n - 1]));
 
             lastOne = new Def()
                 .Match(1).With("it")
@@ -27,16 +30,16 @@ namespace Matchy.Specs
             bottles = new Def()
                 .Match(0).With("no more bottles")
                 .Match(1).With("1 bottle")
-                .Match<int>().With(n=>string.Format("{0} bottles",n));
+                .Match<int>().With(n => string.Format("{0} bottles", n));
         }
 
         [TestMethod]
         public void A_verse()
         {
-            var Expected = 
-                "8 bottles of beer on the wall, 8 bottles of beer.\n" + 
+            var Expected =
+                "8 bottles of beer on the wall, 8 bottles of beer.\n" +
                 "Take one down and pass it around, 7 bottles of beer on the wall.\n";
-            Assert.AreEqual(Expected,verse[8]);
+            Assert.AreEqual(Expected, verse[8]);
         }
 
         [TestMethod]
@@ -55,6 +58,15 @@ namespace Matchy.Specs
                 "2 bottles of beer on the wall, 2 bottles of beer.\n" +
                 "Take one down and pass it around, 1 bottle of beer on the wall.\n";
             Assert.AreEqual(Expected, verse[2]);
+        }
+
+        [TestMethod]
+        public void Verse_0()
+        {
+            var Expected =
+                "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+            Assert.AreEqual(Expected, verse[0]);
         }
     }
 }
